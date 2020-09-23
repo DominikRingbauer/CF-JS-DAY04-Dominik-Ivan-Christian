d0dbea6578741ddd01b711e22513de2af7d7c66f
Ivan Siljic <mail@ivan-siljic.com>
2020-09-23 14:14:52 +0200

    updated

--- Ivan/exercises.js
+++ Ivan/exercises.js
@@ -1,12 +1,17 @@
+/* 	Basic
+	Basic Exercise 1 + 2 */
 
 function basicOne() {
 	var first = document.getElementById("first_name").value;
 	var last = document.getElementById("last_name").value;
 	var x = document.getElementById("age").value;
 
+	var options = document.getElementById("exercise_two").value;
+
 	var result = document.getElementById("display").innerHTML = `Your first name is ${first}!  <br>
 																Your last name is ${last} ! <br>
-																Your age is ${x} ! <br>`;
+																Your age is ${x} ! <br>
+																Your profession is ${options}`;
 
 		if (first.length > 5) {
 			document.getElementById("display").style.color = "green";
@@ -15,7 +20,33 @@ function basicOne() {
 			document.getElementById("display").style.color = "red";
 		}										
 
+		if (options == 'IT') {
+			document.getElementById("display").style.color = "purple";
+		}
+		else if (options == 'Hospitality') {
+			document.getElementById("display").style.color = "yellow";
+		}
+		else {
+			document.getElementById("display").style.color = "blue";
+		}
 }
 
-
 document.getElementById("submit").addEventListener("click", basicOne, false);
+
+
+
+/* 	Intermediate
+	Intermediate exercise 1 | Hotels */
+
+class Hotel {
+	hotelName: "";
+	numberOfNights: "";
+	adress: "";
+	image: "";
+
+
+
+	showAvailability: function() {
+
+	}
+}
