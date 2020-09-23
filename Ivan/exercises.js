ddd3aa574eb9665f2abb686c139a68ea309fb08a
Ivan Siljic <mail@ivan-siljic.com>
2020-09-23 16:07:02 +0200

    added intermediate

--- Ivan/exercises.js
+++ Ivan/exercises.js
@@ -39,14 +39,39 @@ document.getElementById("submit").addEventListener("click", basicOne, false);
 	Intermediate exercise 1 | Hotels */
 
 class Hotel {
-	hotelName: "";
-	numberOfNights: "";
-	adress: "";
-	image: "";
-
-
-
-	showAvailability: function() {
-
-	}
+	hotelName = "";
+	numberOfNights = "";
+	userBooked = "";
+	adress = "";
+	image = "";
+constructor(a, b, c, d, e) {
+	this.hotelName = a;
+	this.numberOfNights = b;
+	this.userBooked = c;
+	this.adress = d;
+	this.image = e;
 }
+
+	showAvailability() {
+        if (this.numberOfNights > this.userBooked) {
+    	   	document.getElementById("reservation").innerHTML = `I/n´ll reserve` 
+       	}
+       else {
+       		document.getElementById("apology").innerHTML = `Sorry, no available nights for ${this.hotelName} !`
+       	}
+ 		}
+
+	displayHotel() {
+			document.getElementById("intermediate_one").innerHTML += `	<h1> ${this.hotelName} </h1>
+																		<p> ${this.adress} </p>
+																		<h3> ${this.numberOfNights} </h3>
+																		<h3> ${this.userBooked} </h3>`;   
+	}
+};
+
+var hotel1 = new Hotel("Hotel California", 2, 3, "larifari 1", NaN);
+hotel1.displayHotel();	
+hotel1.showAvailability();
+
+
+
